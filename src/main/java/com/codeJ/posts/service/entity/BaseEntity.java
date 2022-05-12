88f273148ef683b2bedf04439c4b19a47051c2c0
younjaeyeal <jyyoun1021@gmail.com>
2022-05-13 02:31:35 +0900

    @PrePersist,@PreUpdate 메서드

--- src/main/java/com/codeJ/posts/service/entity/BaseEntity.java
+++ src/main/java/com/codeJ/posts/service/entity/BaseEntity.java
@@ -20,7 +20,7 @@ public abstract class BaseEntity {
     /**
      * 해당 엔티티를 저장하기 이전에 실행
      */
-    @PrePersist//해당 테이블의 insert 연산을 실행할때 같이 실행하라
+    @PrePersist//해당 테이블의 insert 연산을 실행할때 같이 실행
     public void onPrePersist(){
         this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
         this.modifiedDate = this.createdDate;
