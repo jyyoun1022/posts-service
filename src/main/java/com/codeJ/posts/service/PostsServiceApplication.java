87ec2f7d2bc80f395514342f9902568532137746
younjaeyeal <jyyoun1021@gmail.com>
2022-05-13 02:30:45 +0900

    메인 application

--- src/main/java/com/codeJ/posts/service/PostsServiceApplication.java
+++ src/main/java/com/codeJ/posts/service/PostsServiceApplication.java
@@ -6,18 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-/** 추후 빌드 및 배포를 위해 SpringBootServletInitializer 상속 */
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class PostsServiceApplication extends SpringBootServletInitializer {
+public class PostsServiceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PostsServiceApplication.class);
-	}
 
 }
