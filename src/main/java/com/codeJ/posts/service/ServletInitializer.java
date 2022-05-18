1973c245549ed02f20e951d44b2dcaf8b7f09368
younjaeyeal <jyyoun1021@gmail.com>
2022-05-18 15:35:18 +0900

    d

---
+++ src/main/java/com/codeJ/posts/service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.codeJ.posts.service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PostsServiceApplication.class);
+	}
+
+}
